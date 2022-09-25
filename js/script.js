6db6e8790af9df7760b5f550cf5535fca6889232
Luciano Trejos <deftrejos@gmail.com>
2022-09-24 21:51:28 -0300

    img productos + responsive + productos

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+let menu = document.querySelector('#menu');
+let menu_bar = document.querySelector('#menu-bar');
+    
+  menu_bar.addEventListener('click', function() {
+    menu.classList.toggle("menu-toggle");
+    
+  });
